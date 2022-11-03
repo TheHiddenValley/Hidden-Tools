af8015479baa541e4df5596e50873dc1789e3cda
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2022-11-03 16:26:16 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,6 @@ const Footer = () => {
 
     return (
 
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/thehiddenval" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
-                        <svg aria-hidden="true"
-                            focusable="false"
-                            data-prefix="fab"
-                            data-icon="twitter"
-                            className="w-3 h-full mx-auto"
-                            role="img"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
             </div>
 
             <div className="text-center text-white text-sm pb-2">
