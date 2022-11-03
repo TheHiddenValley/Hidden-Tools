1f56ed53f65ba42ceac74be064d8ffc2ca2f631d
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2022-11-03 15:42:22 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="w-100 pb-2">
                 <div className="flex justify-center">
 
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/ToolsSolana" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/thehiddenval" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                         <svg aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
@@ -26,15 +26,6 @@ const Footer = () => {
                         </svg>
                     </a>
 
-                    <a target="_blank" rel="noreferrer" href="https://github.com/cryptoloutre/solana-tools" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
-                        <svg aria-hidden="true"
-                            focusable="false"
-                            data-prefix="fab"
-                            data-icon="github"
-                            className="w-3 h-full mx-auto"
-                            role="img"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 496 512"
                         >
                             <path
                                 fill="currentColor"
@@ -45,15 +36,11 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="text-center text-white pb-2">
-                Try on
-                <a target="_blank" rel="noreferrer" className="text-white" href="https://solanatools-devnet.vercel.app/"> <strong className="underline">Devnet</strong></a>
-            </div>
             <div className="text-center text-white text-sm pb-2">
-                Made by
-                <a target="_blank" rel="noreferrer" className="text-white" href="https://twitter.com/laloutre"> <strong>@laloutre</strong></a>
+                From
+                <a target="_blank" rel="noreferrer" className="text-white" href="https://thehiddenvalley.io"> <strong>The Hidden Valley Team</strong></a>
             </div>
-            <PIPButtonCustom />
+        
         </footer>
     );
 }
