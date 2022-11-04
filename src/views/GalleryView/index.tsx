2f18845bcc226ca65c2d43579820996d6042897a
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2022-11-04 13:40:20 +0800

    Update index.tsx

--- src/views/GalleryView/index.tsx
+++ src/views/GalleryView/index.tsx
@@ -48,7 +48,7 @@ export const GalleryView: FC = ({ }) => {
               <ul className="text-xs sm:text-xl">
                 <li>
                   <Link href="/">
-                    <a>SOLANA-TOOLS</a>
+                    <a>Hidden Tools</a>
                   </Link>
                 </li>
               </ul>
@@ -64,7 +64,7 @@ export const GalleryView: FC = ({ }) => {
             <div className="text-center hero-content w-full">
               <div className="w-full">
                 <h1 className="mb-5 text-5xl">
-                  Burn your Solana <SolanaLogo /> NFTs and get $SOL back
+                NFT Incinerator
                 </h1>
 
                 <div className="w-full min-w-full">
@@ -76,7 +76,7 @@ export const GalleryView: FC = ({ }) => {
                           <input
                             readOnly
                             type="text"
-                            placeholder="Please, connect your wallet"
+                            placeholder="Please connect your wallet"
                             className="w-full input input-bordered input-lg"
                             value={walletToParsePublicKey}
                             style={{
@@ -148,9 +148,8 @@ const NftList = ({ nfts, error, setRefresh }: NftListProps) => {
   return (
     <div>
       <ul className="text-left font-semibold text-base mb-4">
-        <li className=" mb-1"><span className='text-[#16c60c] font-semibold'>✔ Verified</span> : the NFT does not want to drain your wallet. It does not guarantee the quality of the project. It can still be a rug or a poor project.</li>
-        <li className=" mb-1"><span className='text-[#F03A17] font-semibold'>❗ Scam</span> : the NFT wants to drain your wallet. <strong>Do not go on its website</strong> and burn it!</li>
-        <li className=" mb-1"><span className='text-[#ff7f00] font-semibold'><strong>?</strong> No information</span> : not enough information about this NFT. Feel free to send to <a target="_blank" href="https://twitter.com/laloutre"><strong className="text-[#0080FF]">@laloutre</strong></a> the mint address in order to be add in one of the 2 others categories.</li>
+        <li className=" mb-1"><span className='text-[#16c60c] font-semibold'> NFT Incinerator </span> : Burn scam NFTs here and get some SOL back!</li>
+        <li className=" mb-1"><span className='text-[#F03A17] font-semibold'> Choose </span> which NFT you would like to burn. <strong> Make sure you do not burn Official NFTs or you will never get them back. </li>
       </ul>
 
       <BurnButton toBurn={NFTstoBurn} connection={connection} publicKey={publicKey} wallet={wallet} setRefresh={setRefresh} />
