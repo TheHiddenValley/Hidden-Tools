11843741d0bd760e4f7645f5e5035b3896e4d381
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2022-11-04 15:17:37 +0800

    Update index.tsx

--- src/views/GalleryView/index.tsx
+++ src/views/GalleryView/index.tsx
@@ -47,7 +47,7 @@ export const GalleryView: FC = ({ }) => {
             <div className="text-sm breadcrumbs">
               <ul className="text-xs sm:text-xl">
                 <li>
-                  <Link href="/">
+                  <Link href="https://www.thehiddenvalley.io/utility">
                     <a>Hidden Tools</a>
                   </Link>
                 </li>
