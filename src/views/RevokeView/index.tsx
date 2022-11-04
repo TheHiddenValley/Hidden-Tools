f27ce069d50c5c21736dfaceef9dae6f086e395c
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2022-11-04 14:15:01 +0800

    Update index.tsx

--- src/views/RevokeView/index.tsx
+++ src/views/RevokeView/index.tsx
@@ -78,7 +78,7 @@ export const RevokeView: FC = ({ }) => {
                           <input
                             readOnly
                             type="text"
-                            placeholder="Please, connect your wallet"
+                            placeholder="Please connect your wallet"
                             className="w-full input input-bordered input-lg"
                             value={walletToParsePublicKey}
                             style={{
